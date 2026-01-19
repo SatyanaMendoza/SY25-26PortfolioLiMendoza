c06b4982fb434f78a349b459d7ad6134dd267a87
SatyanaMendoza <b2029smsmendoza@pshs.edu.ph>
2026-01-19 08:22:27 +0800

    trying :(

--- public/index.js
+++ public/index.js
@@ -1,4 +1,14 @@
-function toggleMenu() {
-    const menu = document.getElementById('dropdown');
+function toggleMenu1() {
+    const menu = document.getElementById('dropdown1');
     menu.classList.toggle('active');
-  }
+}
+
+function toggleMenu2() {
+    const menu = document.getElementById('dropdown2');
+    menu.classList.toggle('active');
+}
+
+function toggleMenu3() {
+    const menu = document.getElementById('dropdown3');
+    menu.classList.toggle('active');
+}
